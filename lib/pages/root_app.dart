58034893b1468fec955c76cd1730501a6d7a9cba
Kaozhi Chen <kylerchen16@gmail.com>
2024-10-15 17:26:16 -0400

    defined the layout of the app: router and nav bar

---
+++ lib/pages/root_app.dart
@@ -0,0 +1,43 @@
+import 'package:fitness_tracker_app/pages/home_page.dart';
+import 'package:fitness_tracker_app/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({super.key});
+
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: const [
+          HomePage(),
+          // here to display different pages, do this like home page
+          Center(child: Text('Chart Page')),
+          Center(child: Text('Profile Page')),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: pageIndex,
+        onTap: (index) {
+          setState(() {
+            pageIndex = index;
+          });
+        },
+        selectedItemColor: thirdColor,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.pie_chart), label: 'Chart'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/router.dart
@@ -0,0 +1,16 @@
+import 'package:fitness_tracker_app/pages/login_page.dart';
+import 'package:fitness_tracker_app/pages/root_app.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings setting) {
+  switch (setting.name) {
+    case '/login':
+      return MaterialPageRoute(builder: (context) => const LoginPage());
+    case '/root_app':
+      return MaterialPageRoute(builder: (context) => const RootApp());
+    default:
+      return MaterialPageRoute(
+          builder: (context) =>
+              const Scaffold(body: Center(child: Text('Page not found'))));
+  }
+}
