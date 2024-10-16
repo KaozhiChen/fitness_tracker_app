cfdad8db6e7900dd6059fd29c658f783ea07704c
Kaozhi Chen <kylerchen16@gmail.com>
2024-10-15 22:13:27 -0400

    implement the layout of login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:fitness_tracker_app/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -26,91 +27,199 @@ class _LoginPageState extends State<LoginPage> {
           child: Center(
             child: Column(
               children: [
-                const Text(
-                  "Hey there,",
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                const Text(
-                  "Welcome Back!",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                // Welcome and TextField
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    const Text(
+                      "Hey there,",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text(
+                      "Welcome Back!",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      height: 50,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: bgTextField,
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.mail_outline,
+                              color: black.withOpacity(0.5),
+                            ),
+                            const SizedBox(width: 12),
+                            Flexible(
+                                child: TextField(
+                              decoration: const InputDecoration(
+                                  labelText: "Email", border: InputBorder.none),
+                              cursorColor: black.withOpacity(0.5),
+                            ))
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 50,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: bgTextField,
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.lock_outline,
+                              color: black.withOpacity(0.5),
+                            ),
+                            const SizedBox(width: 12),
+                            Flexible(
+                                child: TextField(
+                              obscureText: _obscureText,
+                              decoration: InputDecoration(
+                                  labelText: "Password",
+                                  border: InputBorder.none,
+                                  suffixIcon: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _obscureText = !_obscureText;
+                                        });
+                                      },
+                                      icon: Icon(_obscureText
+                                          ? Icons.visibility
+                                          : Icons.visibility_off_outlined))),
+                              cursorColor: black.withOpacity(0.5),
+                            ))
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Forget Password?",
+                      style: TextStyle(
+                          decoration: TextDecoration.underline, fontSize: 14),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
                 ),
                 const SizedBox(
                   height: 30,
                 ),
-                Container(
-                  height: 50,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: bgTextField,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.mail_outline,
-                          color: black.withOpacity(0.5),
+
+                // login area
+                Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushNamedAndRemoveUntil(
+                            context, "/root_app", (route) => false);
+                      },
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                                colors: [secondary, primary]),
+                            borderRadius: BorderRadius.circular(30)),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: white,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
                         ),
-                        const SizedBox(width: 12),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Row(
+                      children: [
                         Flexible(
-                            child: TextField(
-                          decoration: const InputDecoration(
-                              labelText: "Email", border: InputBorder.none),
-                          cursorColor: black.withOpacity(0.5),
-                        ))
+                            child: Divider(
+                          thickness: 0.8,
+                        )),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text("Or"),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Flexible(
+                            child: Divider(
+                          thickness: 0.8,
+                        )),
                       ],
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 50,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: bgTextField,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.lock_outline,
-                          color: black.withOpacity(0.5),
-                        ),
-                        const SizedBox(width: 12),
-                        Flexible(
-                            child: TextField(
-                          obscureText: _obscureText,
-                          decoration: InputDecoration(
-                              labelText: "Password",
-                              border: InputBorder.none,
-                              suffixIcon: IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _obscureText = !_obscureText;
-                                    });
-                                  },
-                                  icon: Icon(_obscureText
-                                      ? Icons.visibility
-                                      : Icons.visibility_off_outlined))),
-                          cursorColor: black.withOpacity(0.5),
-                        ))
-                      ],
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Forget Password?",
-                  style: TextStyle(
-                      decoration: TextDecoration.underline, fontSize: 14),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              border:
+                                  Border.all(color: black.withOpacity(0.1))),
+                          child: Center(
+                            child: SvgPicture.asset(
+                              "assets/images/google_icon.svg",
+                              width: 20,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              border:
+                                  Border.all(color: black.withOpacity(0.1))),
+                          child: Center(
+                            child: SvgPicture.asset(
+                              "assets/images/facebook_icon.svg",
+                              width: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 )
               ],
             ),
